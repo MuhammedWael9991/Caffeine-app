3972b7b542ddb719c4d6dc1757f5b68195d35c32
MuhammedWael9991 <mw26200400@gmail.com>
2025-07-03 18:35:26 +0300

    Add CaffeineCircleShape composable

---
+++ app/src/main/java/com/caffeine/presentation/components/CaffeineCircleShape.kt
@@ -0,0 +1,45 @@
+package com.caffeine.presentation.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.caffeine.R
+import com.caffeine.ui.theme.white
+
+@Composable
+fun CaffeineCircleShape(
+    modifier: Modifier = Modifier,
+    icon: Int = 0
+){
+    Box(
+        modifier = modifier
+            .size(48.dp)
+            .clip(CircleShape)
+            .background(white)
+    ){
+        Image(
+            painter = painterResource(icon),
+            contentDescription = null,
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun Preview(){
+    Column {
+        CaffeineCircleShape(icon = R.drawable.ic_ghost)
+        CaffeineCircleShape(icon = R.drawable.ic_plus)
+    }
+}
