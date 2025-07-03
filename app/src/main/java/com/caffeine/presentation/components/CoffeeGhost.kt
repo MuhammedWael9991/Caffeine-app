a804f091cdcafd28b207670ccf0c733c8b2daeda
MuhammedWael9991 <mw26200400@gmail.com>
2025-07-03 18:24:37 +0300

    Add CoffeeGhost composable

--- app/src/main/java/com/caffeine/presentation/components/CoffeeGhost.kt
+++ app/src/main/java/com/caffeine/presentation/components/CoffeeGhost.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
